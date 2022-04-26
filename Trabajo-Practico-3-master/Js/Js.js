8510ffa8616180eb9410aa8b9a55edfd4f48b053
Reverie22 <82416940+Reverie22@users.noreply.github.com>
2022-04-26 03:23:38 -0300

    e
    e

---
+++ Trabajo-Practico-3-master/Js/Js.js
@@ -0,0 +1,10 @@
+
+
+
+var principal = document.getElementById('Prueba')
+var Etiquetah1 = document.createElement('h1')
+//verificamos si funciona el codigo//
+console.log(Etiquetah1);
+//ingresamos informacion en la etiqueta//
+Etiquetah1.innerHTML = "HOLA"
+Prueba.appendChild(Etiquetah1)
